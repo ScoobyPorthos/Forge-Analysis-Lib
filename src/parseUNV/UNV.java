9568e1b581a5819738416cb299028dc696e1dbff
Scooby Porthos <scoobyporthos@gmail.com>
2018-08-02 08:42:29 -0400

    Add VTF features

--- src/parseUNV/UNV.java
+++ src/parseUNV/UNV.java
@@ -12,7 +12,6 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
--- src/parseVTF/VTF.java
+++ src/parseVTF/VTF.java
@@ -12,57 +12,77 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
+import traitement.Results;
 
 public class VTF implements Cloneable{
 
 	public static void main(String[] args) {
 
+
 		ArgumentParser parser = ArgumentParsers.newFor("VTFTool").build()
                 .defaultHelp(true)
-                .description("Extract Data from a VTF file");
-        parser.addArgument("-s", "--selector")
-                .help("field ID to extact with comma separated value");
-        parser.addArgument("file").nargs("*")
-                .help("File to calculate checksum");
+                .description("Extract Data from a VTF file into a separate file");
+		
+		parser.addArgument("-a","--action")
+			.choices("variablesNames","data","coordinates")
+			.setDefault("VariablesNames")
+			.help("What do you want to do ?");
+
+		parser.addArgument("-s", "--selector")
+				.setDefault(Arguments.SUPPRESS)
+                .help("field ID to extact with comma separated value ([-a Data] need to be selected)");
+
+		parser.addArgument("file")
+				.nargs("+")
+                .help("VTF file");
         
         
         Namespace ns = null;
         try {
             ns = parser.parseArgs(args);
+            
+            switch (ns.get("action").toString()) {
+    		case "data":
+    			ArrayList<Integer> selection = new ArrayList<>();
+    			String [] selector = ns.get("selector").toString().split(",");
+    			selection.addAll(IntStream.range(0, selector.length).filter(i->!selector[i].isEmpty()).mapToObj(i->Integer.parseInt(selector[i])).collect(Collectors.toList()));
+    			
+    			VTF vtfFile;
+    			for(String name : ns.<String> getList("file"))
+    			{
+    				File file = new File(name);
+    				if(selection.isEmpty())
+    					vtfFile = new VTF(file);
+    				else
+    					vtfFile = new VTF(file,selection);
+
+    				vtfFile.save(file.getParent()+"\\"+file.getName()+".csv");
+    			}
+    			break;
+    		case "variablesNames":
+    			Results variableNames = new Results(new File(ns.<String> getList("file").get(0).toString()));
+    			variableNames.getVariablesNames();
+    			break;
+    		case "coordinates":
+    			Results coordinates = new Results(new File(ns.<String> getList("file").get(0).toString()));
+    			coordinates.getCoordinates();
+    			break;
+    		default:
+    			break;
+    		}
+           
+            
         } catch (ArgumentParserException e) {
             parser.handleError(e);
-            System.exit(1);
         }
         
-		//String path = "D:\\Users\\Scooby\\Documents\\ETS\\LFW\\1.3 - Forge\\vtf\\ChainSim-180116\\";
-		
-		ArrayList<Integer> selection = new ArrayList<>();
-		String [] selector = ns.get("selector").toString().split(",");
-		selection.addAll(IntStream.range(0, selector.length).filter(i->!selector[i].isEmpty()).mapToObj(i->Integer.parseInt(selector[i])).collect(Collectors.toList()));
-		
-		VTF vtfFile;
-		for(String name : ns.<String> getList("file"))
-		{
-			File file = new File(name);
-			if(selection.isEmpty())
-				vtfFile = new VTF(file);
-			else
-				vtfFile = new VTF(file,selection);
-
-			vtfFile.save(file.getParent()+"\\"+file.getName()+".csv");
-		}
-		
-		//VTF friction = new VTF(new File(path+"upper_die-friction.vtf"),selection);
-		//VTF friction = new VTF(new File(path+"upper_die-friction.vtf"));
-		//friction.save(path+"friction.csv");
-		/*VTF deceleration = new VTF(new File(path+"upper_die-deceleration.vtf"));
-		deceleration.save(path+"deceleration.csv");
-		VTF forgeing = new VTF(new File(path+"upper_die-forgeing.vtf"));
-		forgeing.save(path+"forgeing.csv");*/
-		
+        
+        
+        
 	}
 	
 	
@@ -70,7 +90,8 @@ public class VTF implements Cloneable{
 	//private ArrayList<String> type = new ArrayList<>();
 	private ArrayList<String> subjects = new ArrayList<>();
 	private ArrayList<Integer> selection = new ArrayList<>();
-	private HashMap<Integer, Line> content = new HashMap<>();
+	public HashMap<Integer, Line> content = new HashMap<>();
+	public int dataLine = 0;
 	
 	public VTF()
 	{
@@ -129,7 +150,9 @@ public class VTF implements Cloneable{
 				else if(line.trim().equals("%VARIABLE_NAMES"))
 					action = 2;
 				else if(line.trim().equals("%DATA"))
+				{
 					action = 3;
+				}
 				else if(action !=3)
 					action = 0;
 				
@@ -192,7 +215,7 @@ public class VTF implements Cloneable{
 		FileWriter fr;
 		try {
 			fr = new FileWriter(name);
-			fr.write(this.subjects.stream().map(Object::toString).collect(Collectors.joining(", ")));
+			fr.write(this.subjects.stream().map(s->s.replaceAll("\"","").trim().replaceAll("\\s","_")).collect(Collectors.joining(",")));
 	    	fr.write("\n");
 	    	fr.write(this.content.values().stream().map(i-> i.getCSVLine()).collect(Collectors.joining("\n")));
 	        fr.close();
@@ -200,5 +223,9 @@ public class VTF implements Cloneable{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}    	
+	}
+	public void getData()
+	{
+		System.out.println(IntStream.range(0, this.subjects.size()).mapToObj(i -> i+" -> "+this.getSubjects().get(i).substring(1, this.getSubjects().get(i).length()-1)).collect(Collectors.joining("\n")));
 	}
 }
