cbb02211bdaf09c3ade602dffcc553581b6c0ee9
Scooby Porthos <scoobyporthos@gmail.com>
2018-11-26 10:51:57 -0500

    CleanUP VTF to be more efficient

--- src/parseVTF/VTF.java
+++ src/parseVTF/VTF.java
@@ -1,14 +1,19 @@
 package parseVTF;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import net.sourceforge.argparse4j.ArgumentParsers;
@@ -16,9 +21,8 @@ import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
-import traitement.Results;
 
-public class VTF implements Cloneable{
+public class VTF{
 
 	public static void main(String[] args) {
 
@@ -27,49 +31,113 @@ public class VTF implements Cloneable{
                 .defaultHelp(true)
                 .description("Extract Data from a VTF file into a separate file");
 		
-		parser.addArgument("-a","--action")
+		parser.addArgument("-g","--get")
 			.choices("variablesNames","data","coordinates")
 			.setDefault("VariablesNames")
 			.help("What do you want to do ?");
+		
+		parser.addArgument("--thread")
+			.setDefault(10)
+			.help("thread number used to process the data");
 
 		parser.addArgument("-s", "--selector")
 				.setDefault(Arguments.SUPPRESS)
                 .help("field ID to extact with comma separated value ([-a Data] need to be selected)");
+		
+		parser.addArgument("-f", "--files")
+			.action(Arguments.storeTrue())
+			.setDefault(false)
+	        .help("Is the inputs are files");
+		
+		parser.addArgument("-d", "--directories")
+			.action(Arguments.storeTrue())
+			.setDefault(false)
+			.help("Is the inputs are directories");
 
-		parser.addArgument("file")
+		parser.addArgument("inputs")
 				.nargs("+")
-                .help("VTF file");
+                .help("VTF file or VTF directories");
         
         
         Namespace ns = null;
         try {
             ns = parser.parseArgs(args);
-            
-            switch (ns.get("action").toString()) {
+            switch (ns.get("get").toString()) {
     		case "data":
-    			ArrayList<Integer> selection = new ArrayList<>();
+    			ExecutorService service = Executors.newFixedThreadPool(Integer.parseInt(ns.get("thread").toString()));
     			String [] selector = ns.get("selector").toString().split(",");
-    			selection.addAll(IntStream.range(0, selector.length).filter(i->!selector[i].isEmpty()).mapToObj(i->Integer.parseInt(selector[i])).collect(Collectors.toList()));
+    			List<Integer> selection  = IntStream.range(0, selector.length).filter(i->!selector[i].isEmpty()).mapToObj(i->Integer.parseInt(selector[i])).collect(Collectors.toList());
     			
-    			VTF vtfFile;
-    			for(String name : ns.<String> getList("file"))
+    			if(ns.getBoolean("files")) {
+    				List<String> files = ns.getList("inputs");
+    				
+    				IntStream.range(0, files.size()).forEach(i -> service.submit(() -> {
+                    	
+                    	System.out.println("Task ID : " + i + " started by "+ Thread.currentThread().getName()+"...");
+                    	VTF data;
+                    	File file =  new File(files.get(i));
+                    	if(selection.isEmpty())
+                    		data = new VTF(file);
+                    	else
+                    		data = new VTF(file,selection);
+            			
+            			data.save(files.get(i)+".csv");
+            			System.out.println("Task ID : " + i + " terminated");
+            			
+                    }));
+            		service.shutdown();
+    			}
+    			else if(ns.getBoolean("directories"))
     			{
-    				File file = new File(name);
-    				if(selection.isEmpty())
-    					vtfFile = new VTF(file);
-    				else
-    					vtfFile = new VTF(file,selection);
-
-    				vtfFile.save(file.getParent()+"\\"+file.getName()+".csv");
+    				List<String> dirs = ns.getList("inputs");
+    				
+    				for(String dir : dirs)
+    				{
+    					System.out.println(dir);
+    					File[] files = new File(dir).listFiles(VTF.vtfFilter);
+    					
+    					IntStream.range(0, files.length).forEach(i -> service.submit(() -> {
+                        	
+                        	System.out.println("Task ID : " + i + " started by "+ Thread.currentThread().getName()+"...");
+                        	VTF data;
+                        	if(selection.isEmpty())
+                        		data = new VTF(files[i]);
+                        	else
+                        		data = new VTF(files[i],selection);
+                			
+                			data.save(files[i].getPath()+".csv");
+                			System.out.println("Task ID : " + i + " terminated");
+                			
+                        }));
+                		service.shutdown();
+    				}
+    				System.out.println();
     			}
     			break;
     		case "variablesNames":
-    			VTF variableNames = new VTF(new File(ns.<String> getList("file").get(0).toString()));
-    			variableNames.getData();
+    			if(ns.getBoolean("files")) {
+    				VTF variableNames = new VTF(new File(ns.getList("inputs").get(0).toString()));
+        			variableNames.getData();
+    			}
+    			else if(ns.getBoolean("directories"))
+    			{
+    				List<String> dirs = ns.getList("inputs");
+    				
+    				for(String dir : dirs)
+    				{
+    					System.out.println(dir);
+    					File[] files = new File(dir).listFiles(VTF.vtfFilter);
+        				VTF variableNames = new VTF(files[0]) ;
+            			variableNames.getData();
+    				}
+    				System.out.println();
+    			}
+    			else
+    				System.out.println("Please specify if you are using files inputs [-f] or directories inputs [-d] ");
     			break;
     		case "coordinates":
-    			Results coordinates = new Results(new File(ns.<String> getList("file").get(0).toString()));
-    			coordinates.getCoordinates();
+    			//Results coordinates = new Results(new File(ns.<String> getList("file").get(0).toString()));
+    			//coordinates.getCoordinates();
     			break;
     		default:
     			break;
@@ -80,134 +148,67 @@ public class VTF implements Cloneable{
             parser.handleError(e);
         }
         
-        
-        
-        
 	}
 	
 	
 	private File file;
-	//private ArrayList<String> type = new ArrayList<>();
-	private ArrayList<String> subjects = new ArrayList<>();
-	private ArrayList<Integer> selection = new ArrayList<>();
-	public HashMap<Integer, Line> content = new HashMap<>();
+	private List<String> subjects = new ArrayList<>();
+	private List<Integer> selection = new ArrayList<>();
+	public List<List<Double>> content = new ArrayList<>();
 	public int dataLine = 0;
 	
-	public VTF()
-	{
-		
-	}
+	public Function<String, List<String>> splitVTFstring = (line) -> {
+	  	  String[] p = line.split("\t");
+	  	  return IntStream.range(0, p.length).filter(i-> this.selection.isEmpty() || this.selection.contains(i)).mapToObj(i->p[i].replace('\"', ' ').trim().replace(' ', '_')).collect(Collectors.toList());
+	  	};
+  	public static Function<String, Double> parseDouble = (s) -> {
+  		Double d;
+  		try{
+  	      d = Double.parseDouble(s);
+  	    }catch(NumberFormatException nfe){
+  	      d = Double.NaN;
+  	    }
+  		return d;
+	  	};
+  	public Function<String, List<Double>> splitVTFnum = (line) -> {
+	  	  String[] p = line.split("\t");
+	  	  return IntStream.range(0, p.length).filter(i-> this.selection.isEmpty() || this.selection.contains(i)).mapToObj(i->p[i]).map(VTF.parseDouble).collect(Collectors.toList());
+	  	};
+	
+  	public static FilenameFilter vtfFilter = new FilenameFilter() {
+		public boolean accept(File dir, String name) {
+			return name.toLowerCase().endsWith(".vtf");
+		}
+	};
 	
 	public VTF(File file)
 	{
 		this.file = file;
 		this.readFile();
 	}
-	public VTF(File file, ArrayList<Integer> selection)
+	public VTF(File file, List<Integer> selection)
 	{
 		this.selection = selection;
 		this.file = file;
 		this.readFile();
 	}
 	
-	public VTF clone() throws CloneNotSupportedException {
-        return (VTF) super.clone();
-    }
-	
 	public File getFile() {
 		return this.file;
 	}
 	private void readFile()
-	{
-		FileInputStream inputStream = null;
-		Scanner sc = null;
-		try {
-			inputStream = new FileInputStream(file.getAbsolutePath());
-			sc = new Scanner(inputStream, "UTF-8");
-		    
-		    int i = 0;
-		    int action = 0;
-		    while (sc.hasNextLine()){
-		    	String line = sc.nextLine();
-				switch(action)
-				{
-				case 1:
-					//this.exctractType(line.trim());
-					break;
-				case 2 : 
-					this.exctractSubject(line.trim());
-					break;
-				case 3:
-					if(this.selection.isEmpty())
-						content.put(i, new Line(i,line.trim(),this.subjects));
-					else
-						content.put(i, new Line(i,line.trim(),this.subjects,this.selection));
-					i++;
-					break;
-				}
-				if(line.trim().equals("%VARIABLE_TYPES"))
-					action = 1;
-				else if(line.trim().equals("%VARIABLE_NAMES"))
-					action = 2;
-				else if(line.trim().equals("%DATA"))
-				{
-					action = 3;
-				}
-				else if(action !=3)
-					action = 0;
-				
-		    }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} 
+	{   	 
+	    try{
+	      InputStream inputFS = new FileInputStream(this.file);
+	      BufferedReader br = new BufferedReader(new InputStreamReader(inputFS));
+	      this.subjects = br.lines().skip(7).findFirst().map(this.splitVTFstring).get();
+	      this.content =  br.lines().skip(9).map(this.splitVTFnum).collect(Collectors.toList());
+	      br.close();
+	    } catch (IOException e) {
+	    	e.printStackTrace();
+	    }
 	}
-	/*private void exctractType(String line)
-	{
-		List<String> subject = IntStream.range(0,line.split("\t").length)
-				.filter(i -> !line.split("\t")[i].isEmpty())
-				.mapToObj(i -> line.split("\t")[i])
-				.collect(Collectors.toList());
-		this.type.addAll(subject);
-	}*/
-	private void exctractSubject(String line)
-	{
-		if(!this.selection.isEmpty())
-		{
-			List<String> subject = IntStream.range(0,line.split("\t").length)
-					.filter(i -> this.selection.contains(i+1) && !line.split("\t")[i].isEmpty())
-					.mapToObj(i -> line.split("\t")[i].trim())
-					.collect(Collectors.toList());
-			this.subjects.addAll(subject);
-		}
-		else
-		{
-			List<String> subject = IntStream.range(0,line.split("\t").length)
-					.filter(i -> !line.split("\t")[i].isEmpty())
-					.mapToObj(i -> line.split("\t")[i].trim())
-					.collect(Collectors.toList());
-			this.subjects.addAll(subject);
-		}
-	}
-	public HashMap<Integer, Line> getColumn(ArrayList<String> column)
-	{
-		HashMap<Integer, Line> values = new HashMap<>();
-		this.content.entrySet().stream().forEach(entry -> values.put(entry.getKey(), entry.getValue().extract(column)));
-		return values;
-	}
-	public HashMap<Integer, Line> getColumn()
-	{
-		HashMap<Integer, Line> values = new HashMap<>();
-		this.content.entrySet().stream().forEach(entry -> values.put(entry.getKey(), entry.getValue().extract()));
-		return values;
-	}
-	public void addColumns(HashMap<Integer, Line> colmuns)
-	{
-		if(this.content.isEmpty())
-			colmuns.entrySet().stream().forEach(entry -> this.content.put(entry.getKey(),entry.getValue()));
-		else
-			colmuns.entrySet().stream().forEach(entry -> this.content.get(entry.getKey()).addCell(entry.getValue().getData()));
-	}
-	public ArrayList<String> getSubjects() {
+	public List<String> getSubjects() {
 		return this.subjects;
 	}
 	public void save(String name)
@@ -215,9 +216,9 @@ public class VTF implements Cloneable{
 		FileWriter fr;
 		try {
 			fr = new FileWriter(name);
-			fr.write(this.subjects.stream().map(s->s.replaceAll("\"","").trim().replaceAll("\\s","_")).collect(Collectors.joining(",")));
+			fr.write(this.subjects.stream().collect(Collectors.joining(",")));
 	    	fr.write("\n");
-	    	fr.write(this.content.values().stream().map(i-> i.getCSVLine()).collect(Collectors.joining("\n")));
+	    	fr.write(this.content.stream().map(l-> l.stream().map(Objects::toString).collect(Collectors.joining(","))).collect(Collectors.joining("\n")));
 	        fr.close();
 	        System.out.println(name+" written");
 		} catch (IOException e) {
@@ -226,6 +227,6 @@ public class VTF implements Cloneable{
 	}
 	public void getData()
 	{
-		System.out.println(IntStream.range(0, this.subjects.size()).mapToObj(i -> (i+1)+" -> "+this.getSubjects().get(i).substring(1, this.getSubjects().get(i).length()-1)).collect(Collectors.joining("\n")));
+		System.out.println(IntStream.range(0, this.subjects.size()).mapToObj(i -> i+"\t->\t"+this.subjects.get(i)).collect(Collectors.joining("\n")));
 	}
 }
